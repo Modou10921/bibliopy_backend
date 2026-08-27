5063f59d3de3454fc70eac18709006e54d93ab71
Modou10921 <oluodom418@gmail.com>
2026-08-27 14:25:47 +0000

    Correction de la casse du module settings

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliopy_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BIBLIOPY_BACKEND.settings')
 
 application = get_wsgi_application()
