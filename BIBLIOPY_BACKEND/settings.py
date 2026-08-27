ab0818eda1b2d2a24a499cf1fe8aa30bc96f5df1
Modou10921 <oluodom418@gmail.com>
2026-08-27 15:05:58 +0000

    Ajout de WhiteNoise et configuration des fichiers statiques

--- BIBLIOPY_BACKEND/settings.py
+++ BIBLIOPY_BACKEND/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # À AJOUTER ICI
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # PLACÉ ICI AVEC LES GUILLEMETS ET LA VIRGULE
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,3 +149,4 @@ REST_FRAMEWORK = {
 }
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
